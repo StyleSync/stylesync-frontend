b1e0776bf24fab4d12eac3eaf65b6cd73a0d5550
VadimTarasenko <vadym.tarasenko99@gmail.com>
2023-06-28 13:29:36 +0300

    feat: add radio button component

---
+++ src/modules/core/components/radio-button/radio-button.interface.ts
@@ -0,0 +1,17 @@
+import { ChildrenProp } from '@/modules/core/types/react.types';
+
+export type RadioButtonProps = {
+  value: string;
+};
+
+export type RadioGroupProps = ChildrenProp & {
+  name: string;
+  value: null | string;
+  onChange: (value: string) => void;
+};
+
+export type RadioGroupContextValue = {
+  activeValue: null | string;
+  name: string;
+  onChange: (value: string) => void;
+};
---
+++ src/modules/core/components/radio-button/radio-button.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { FC, useState } from 'react';
+import { Typography } from '@/modules/core/components/typogrpahy';
+
+import { RadioButton } from './radio-button';
+
+const meta: Meta<typeof RadioButton> = {
+  title: 'Core UI/Radio button',
+  component: RadioButton,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof RadioButton>;
+
+const BaseCardWithRadioButton: FC<{
+  value: string;
+  title: string;
+  onClick?: (name: string) => void;
+}> = ({ value, title, onClick }) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        padding: '4px 20px 4px 10px',
+        columnGap: '5px',
+        borderRadius: '6px',
+        boxShadow: '0px 4px 15px 0px rgba(37, 74, 165, 0.10)',
+        cursor: onClick ? 'pointer' : 'unset',
+      }}
+      onClick={() => onClick && onClick(value)}
+    >
+      <RadioButton value={value} />
+      <Typography>{title}</Typography>
+    </div>
+  );
+};
+
+export const Base: Story = {
+  render: () => {
+    const [value, setValue] = useState<string | null>(null);
+
+    return (
+      <RadioButton.Group value={value} onChange={setValue} name='cards'>
+        <div style={{ display: 'grid', rowGap: '20px', width: 'fit-content' }}>
+          <BaseCardWithRadioButton value='card1' title='Card 1' />
+          <BaseCardWithRadioButton value='card2' title='Card 2' />
+          <BaseCardWithRadioButton
+            value='card3'
+            title='Card 3 (Click by card to select)'
+            onClick={setValue}
+          />
+        </div>
+      </RadioButton.Group>
+    );
+  },
+};
