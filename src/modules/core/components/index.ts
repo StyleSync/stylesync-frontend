34d6e8ef6e9d10dd16b37c2ec075eff54e6fcb62
VadimTarasenko <vadym.tarasenko99@gmail.com>
2023-07-11 22:03:13 +0300

    feat: add tabs component

--- src/modules/core/components/index.ts
+++ src/modules/core/components/index.ts
@@ -1,2 +1,3 @@
 export * from './icon';
 export * from './typogrpahy';
+export * from './tabs';
---
+++ src/modules/core/components/tabs/tabs.tsx
@@ -0,0 +1,64 @@
+import { type CSSProperties, type FC, useMemo, useState } from 'react';
+import clsx from 'clsx';
+// components
+import { Icon, Typography } from '@/modules/core/components';
+
+import type { TabsProps } from './tabs.interface';
+import styles from './tabs.module.scss';
+
+export const Tabs: FC<TabsProps> = ({
+  value,
+  tabs,
+  onChange,
+  typographyProps,
+}) => {
+  // state
+  const [activeTabElement, setActiveTabElement] =
+    useState<HTMLDivElement | null>(null);
+  // memo
+  const indicatorStyles = useMemo<CSSProperties>(() => {
+    if (!activeTabElement) {
+      return {};
+    }
+
+    return {
+      left: activeTabElement.offsetLeft,
+      width: activeTabElement.clientWidth,
+    };
+  }, [activeTabElement]);
+
+  const handleTabClick = (key: string) => () => {
+    if (value !== key) {
+      onChange && onChange(key);
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      {tabs.map((tab) => (
+        <div
+          key={tab.key}
+          ref={(element) => {
+            if (element && tab.key === value) {
+              setActiveTabElement(element);
+            }
+          }}
+          className={clsx(styles.tab, {
+            [styles.tab_active]: value === tab.key,
+          })}
+          onClick={handleTabClick(tab.key)}
+        >
+          {tab.icon && <Icon name={tab.icon} className={styles.icon} />}
+          <Typography
+            weight='medium'
+            className={styles.tabName}
+            {...typographyProps}
+          >
+            {tab.name}
+          </Typography>
+        </div>
+      ))}
+      <div className={styles.indicator} style={indicatorStyles} />
+    </div>
+  );
+};
