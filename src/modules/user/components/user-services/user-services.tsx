dfbd0ce69fad56edb91353f7aae6902770066f99
Eugene Derii <eugenederii18@gmail.com>
2023-08-24 17:17:21 +0300

    feat: add services section

---
+++ src/modules/user/components/user-services/user-services.tsx
@@ -0,0 +1,11 @@
+'use client';
+import clsx from 'clsx';
+
+// components
+import {} from '@/modules/core/components';
+
+import styles from './user-services.module.scss';
+
+export const Services = () => {
+  return <div>my services</div>;
+};
