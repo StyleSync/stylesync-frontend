34deb399283bda8fcf6bd97e2d9213890975950a
VadimTarasenko <35116343+VadimTarasenko@users.noreply.github.com>
2023-08-28 12:35:01 +0300

    feat: setup gallery grid (#25)

---
+++ src/modules/core/components/gallery/gallery.interface.ts
@@ -0,0 +1,11 @@
+export type GalleryImage = {
+  src: string;
+  original: string;
+  width: number;
+  height: number;
+  caption?: string;
+};
+
+export type GalleryProps = {
+  images: GalleryImage[];
+};
---
+++ src/modules/core/components/gallery/gallery.tsx
@@ -0,0 +1,46 @@
+import { type FC, useCallback, useMemo, useState } from 'react';
+import { Gallery as ReactGridGallery } from 'react-grid-gallery';
+import Lightbox from 'yet-another-react-lightbox';
+import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
+import 'yet-another-react-lightbox/styles.css';
+import 'yet-another-react-lightbox/plugins/thumbnails.css';
+
+import type { GalleryProps } from './gallery.interface';
+import './gallery.global.scss';
+
+export const Gallery: FC<GalleryProps> = ({ images }) => {
+  const [index, setIndex] = useState<number>(-1);
+  // memo
+  const slides = useMemo(
+    () =>
+      images.map(({ original }) => ({
+        src: original,
+      })),
+    [images]
+  );
+
+  const handleImageClick = useCallback((i: number) => {
+    setIndex(i);
+  }, []);
+
+  return (
+    <>
+      <ReactGridGallery
+        images={images}
+        onClick={handleImageClick}
+        enableImageSelection={false}
+      />
+      <Lightbox
+        slides={slides}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        plugins={[Thumbnails]}
+        thumbnails={{
+          borderRadius: 12,
+          imageFit: 'cover',
+        }}
+      />
+    </>
+  );
+};
