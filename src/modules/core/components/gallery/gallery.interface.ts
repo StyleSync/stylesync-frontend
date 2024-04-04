0a7abfd1221fea5d8e910dd0c8c13a58913d91b2
Eugene Derii <eugenederii@gmail.com>
2024-04-04 14:22:12 +0300

    feat: add to display photos on user page and displayed the title and description inside

--- src/modules/core/components/gallery/gallery.interface.ts
+++ src/modules/core/components/gallery/gallery.interface.ts
@@ -1,13 +1,5 @@
 import { type PortfolioExtendedProfessional } from '@/modules/settings/components/settings-gallery/settings-gallery.interface';
 
-export type GalleryImage = {
-  src: string;
-  original: string;
-  width: number;
-  height: number;
-  caption?: string;
-};
-
 export type GalleryProps = {
   images: PortfolioExtendedProfessional[];
   maxRows?: number;
