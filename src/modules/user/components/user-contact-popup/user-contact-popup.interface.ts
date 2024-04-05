436b720e8483fae88b68610ce03e4122a06f5eda
Eugene Derii <eugenederii@gmail.com>
2024-04-05 21:23:17 +0300

    feat: adding logic to the contact icons

--- src/modules/user/components/user-contact-popup/user-contact-popup.interface.ts
+++ src/modules/user/components/user-contact-popup/user-contact-popup.interface.ts
@@ -1,3 +1,8 @@
 import type { PopoverProps } from '@/modules/core/components/popover/popover.interface';
+import type { Prisma } from '@prisma/client';
 
-export type UserContactPopupProps = Omit<PopoverProps, 'children'> & {};
+export type UserContactPopupProps = Omit<PopoverProps, 'children'> & {
+  professional: Prisma.ProfessionalGetPayload<{
+    include: { user: true };
+  }>;
+};
--- src/modules/user/components/user-contact-popup/user-contact-popup.tsx
+++ src/modules/user/components/user-contact-popup/user-contact-popup.tsx
@@ -8,33 +8,66 @@ import type { UserContactPopupProps } from './user-contact-popup.interface';
 import styles from './user-contact-popup.module.scss';
 
 export const UserContactPopup: FC<UserContactPopupProps> = ({ ...props }) => {
+  const handlePhoneClick = () => {
+    if (props.professional && props.professional.user.phone) {
+      window.open(`tel:${props.professional.user.phone}`);
+    }
+  };
+
+  const handleSocialMediaClick = (url?: string | null) => {
+    if (url) {
+      window.open(url, '_blank');
+    }
+  };
+
+  const handleEmailClick = () => {
+    if (props.professional && props.professional.user.email) {
+      location.href = `mailto:${props.professional.user.email}`;
+    }
+  };
+
   return (
     <Popover {...props} sideOffset={5} side='bottom'>
       <div className={styles.root}>
-        <Button
-          icon='phone'
-          className={clsx('focusable', styles.socialBtn, styles.phone)}
-          variant='unstyled'
-          rippleColor={styles.rippleColor}
-        />
-        <Button
-          icon='facebook'
-          className={clsx('focusable', styles.socialBtn, styles.facebook)}
-          variant='unstyled'
-          rippleColor={styles.rippleColor}
-        />
-        <Button
-          icon='instagram'
-          className={clsx('focusable', styles.socialBtn, styles.instagram)}
-          variant='unstyled'
-          rippleColor={styles.rippleColor}
-        />
-        <Button
-          icon='inbox'
-          className={clsx('focusable', styles.socialBtn, styles.mail)}
-          variant='unstyled'
-          rippleColor={styles.rippleColor}
-        />
+        {props.professional && props.professional.user.phone && (
+          <Button
+            onClick={handlePhoneClick}
+            icon='phone'
+            className={clsx('focusable', styles.socialBtn, styles.phone)}
+            variant='unstyled'
+            rippleColor={styles.rippleColor}
+          />
+        )}
+
+        {props.professional && props.professional.facebook && (
+          <Button
+            onClick={() => handleSocialMediaClick(props.professional.facebook)}
+            icon='facebook'
+            className={clsx('focusable', styles.socialBtn, styles.facebook)}
+            variant='unstyled'
+            rippleColor={styles.rippleColor}
+          />
+        )}
+
+        {props.professional && props.professional.instagram && (
+          <Button
+            onClick={() => handleSocialMediaClick(props.professional.instagram)}
+            icon='instagram'
+            className={clsx('focusable', styles.socialBtn, styles.instagram)}
+            variant='unstyled'
+            rippleColor={styles.rippleColor}
+          />
+        )}
+
+        {props.professional && props.professional.user.email && (
+          <Button
+            onClick={handleEmailClick}
+            icon='inbox'
+            className={clsx('focusable', styles.socialBtn, styles.mail)}
+            variant='unstyled'
+            rippleColor={styles.rippleColor}
+          />
+        )}
       </div>
     </Popover>
   );
