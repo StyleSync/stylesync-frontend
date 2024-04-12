a840a9e07a5d9e90b3f787b377549a181c543989
Eugene Derii <eugenederii@gmail.com>
2024-04-12 15:06:08 +0300

    feat: added ts-ignore in gallery.stories

--- src/modules/core/components/gallery/gallery.interface.ts
+++ src/modules/core/components/gallery/gallery.interface.ts
@@ -1,5 +1,13 @@
 import { type PortfolioExtendedProfessional } from '@/modules/settings/components/settings-gallery/settings-gallery.interface';
 
+export type GalleryImage = {
+  src: string;
+  original: string;
+  width: number;
+  height: number;
+  caption?: string;
+};
+
 export type GalleryProps = {
   images: PortfolioExtendedProfessional[];
   maxRows?: number;
