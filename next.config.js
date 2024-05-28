f70087c4e27c3cc8b32f639246c6d3fff7212cc2
Vadim Tarasenko <vadim@wormsconnect.com>
2024-05-28 11:05:18 +0300

    chore: update export method of next js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  output: 'standalone',
   compiler: {
     removeConsole: process.env.NODE_ENV !== 'development', // Remove console.log in production
   },
