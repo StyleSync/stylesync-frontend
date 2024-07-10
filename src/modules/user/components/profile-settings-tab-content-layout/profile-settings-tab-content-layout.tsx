2c5286289d82ec0a02aa9c44f638b27328002b0b
Vadim Tarasenko <vadim@wormsconnect.com>
2024-07-10 18:43:21 +0300

    Display suggestions to complete profile

--- src/modules/user/components/profile-settings-tab-content-layout/profile-settings-tab-content-layout.tsx
+++ src/modules/user/components/profile-settings-tab-content-layout/profile-settings-tab-content-layout.tsx
@@ -1,5 +1,7 @@
 import { type FC, useMemo } from 'react';
 import { useIntl } from 'react-intl';
+import clsx from 'clsx';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 // components
 import { Divider } from '@/modules/core/components/divider';
 import { Button } from '@/modules/core/components/button';
@@ -10,10 +12,14 @@ import { Placeholder } from '@/modules/core/components/placeholder';
 // hooks
 import { useDeviceType } from '@/modules/core/hooks/use-device-type';
 import { useSettingsNavigation } from '@/modules/user/hooks/use-settings-navigation';
+// utils
+import { trpc } from '@/modules/core/utils/trpc.utils';
 // type
 import type { ProfileSettingsTabContentLayoutProps } from './profile-settings-tab-content-layout.interface';
 import type { ButtonProps } from '@/modules/core/components/button/button.interface';
 // style
+import 'react-circular-progressbar/dist/styles.css';
+
 import styles from './profile-settings-tab-content-layout.module.scss';
 
 export const ProfileSettingsTabContentLayout: FC<
@@ -22,6 +28,17 @@ export const ProfileSettingsTabContentLayout: FC<
   const intl = useIntl();
   const deviceType = useDeviceType();
   const { reset } = useSettingsNavigation();
+  // queries
+  const { data: me } = trpc.user.me.useQuery({ expand: ['professional'] });
+  const { data: profileCompletionStatus } =
+    trpc.professional.getProfileCompletionStatus.useQuery(
+      {
+        id: me?.id || '',
+      },
+      {
+        enabled: !!me,
+      }
+    );
   // memo
   const _actions = useMemo<ButtonProps[]>(() => {
     if (deviceType === 'mobile') {
@@ -54,24 +71,77 @@ export const ProfileSettingsTabContentLayout: FC<
           {title}
         </Typography>
       </div>
-      <div className={styles.content}>
-        <div className={styles.scrolledContent}>
-          <Placeholder
-            isActive={isLoading}
-            placeholder={<Spinner size='medium' />}
-          >
-            <div className={styles.childrenWrapper}>{children}</div>
-          </Placeholder>
+      <div className='flex-1 flex gap-x-6'>
+        <div className={styles.content}>
+          <div className={styles.scrolledContent}>
+            <Placeholder
+              isActive={isLoading}
+              placeholder={<Spinner size='medium' />}
+            >
+              <div className={styles.childrenWrapper}>{children}</div>
+            </Placeholder>
+          </div>
+          {_actions.length > 0 && !hideActions && (
+            <>
+              <Divider className={styles.divider} variant='horizontal' />
+              <div className={styles.actions}>
+                {_actions.map((action, index) => (
+                  <Button key={index} {...action} />
+                ))}
+              </div>
+            </>
+          )}
         </div>
-        {_actions.length > 0 && !hideActions && (
-          <>
-            <Divider className={styles.divider} variant='horizontal' />
-            <div className={styles.actions}>
-              {_actions.map((action, index) => (
-                <Button key={index} {...action} />
-              ))}
+        {profileCompletionStatus && !profileCompletionStatus.isAllCompleted && (
+          <div className='w-[300px] bg-white shadow rounded-[20px] p-8 flex flex-col gap-y-6'>
+            <span className='text-dark text-base font-medium'>
+              {intl.formatMessage({
+                id: 'professional.settings.profileRequirements.title',
+              })}
+            </span>
+            <div className='w-[70%] mx-auto'>
+              <CircularProgressbar
+                strokeWidth={6}
+                value={
+                  (profileCompletionStatus.completedCount /
+                    profileCompletionStatus.requirements.length) *
+                  100
+                }
+                styles={buildStyles({
+                  pathColor: '#3b82ef',
+                  trailColor: '#d8e6fc',
+                  textColor: '#3b82ef',
+                })}
+                text={`${Math.floor(
+                  (profileCompletionStatus.completedCount /
+                    profileCompletionStatus.requirements.length) *
+                    100
+                )}%`}
+              />
             </div>
-          </>
+            <div className='flex flex-col gap-y-2'>
+              {profileCompletionStatus.requirements.map(
+                ({ requirementCompleted, requirementTitle }, index) => (
+                  <div
+                    className={clsx('flex items-start gap-x-2 text-gray', {
+                      'text-green': requirementCompleted,
+                    })}
+                    key={index}
+                  >
+                    <Icon
+                      name={requirementCompleted ? 'check-mark' : 'close'}
+                      width={18}
+                      color='inherit'
+                      className='shrink-0 mt-[2px]'
+                    />
+                    <span className='text-inherit text-sm font-medium'>
+                      {intl.formatMessage({ id: requirementTitle })}
+                    </span>
+                  </div>
+                )
+              )}
+            </div>
+          </div>
         )}
       </div>
     </div>
