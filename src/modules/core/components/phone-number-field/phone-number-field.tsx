12faaa26342162d5f1cb3e1ad19a64d5f82cd0bd
Eugene Derii <eugenederii@gmail.com>
2024-07-22 16:53:00 +0300

    feat: add phone-number-field

---
+++ src/modules/core/components/phone-number-field/phone-number-field.tsx
@@ -0,0 +1,18 @@
+// import React, { useState } from 'react';
+// import { PhoneInput } from 'react-international-phone';
+
+// import 'react-international-phone/style.css';
+
+// export const PhoneNumberField = () => {
+//   const [phone, setPhone] = useState('');
+
+//   return (
+//     <div>
+//       <PhoneInput
+//         defaultCountry='ua'
+//         value={phone}
+//         onChange={(phone) => setPhone(phone)}
+//       />
+//     </div>
+//   );
+// };
