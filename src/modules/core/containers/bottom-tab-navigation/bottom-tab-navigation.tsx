f8fcfc5bdf8352c031d6f343a70f9f8b2cb562f0
Vadim Tarasenko <vadim@wormsconnect.com>
2024-10-05 15:01:37 +0300

    feat: display bottom tab navigation on landing page if logged in

--- src/modules/core/containers/bottom-tab-navigation/bottom-tab-navigation.tsx
+++ src/modules/core/containers/bottom-tab-navigation/bottom-tab-navigation.tsx
@@ -5,14 +5,17 @@ import { usePathname, useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 // components
 import { Button } from '@/modules/core/components/button';
+// containers
+import { BottomFixedContent } from '@/modules/core/containers/bottom-fixed-content';
+// hooks
+import { useDeviceType } from '@/modules/core/hooks/use-device-type';
+// utils
+import { trpc } from '@/modules/core/utils/trpc.utils';
 // types
 import type { IconName } from '@/modules/core/components/icon';
 
 import type { BottomTabNavigationProps } from './bottom-tab-navigation.interface';
 import styles from './bottom-tab-navigation.module.scss';
-import { BottomFixedContent } from '@/modules/core/containers/bottom-fixed-content';
-import { trpc } from '@/modules/core/utils/trpc.utils';
-import { useDeviceType } from '@/modules/core/hooks/use-device-type';
 
 export const BottomTabNavigation: FC<BottomTabNavigationProps> = () => {
   const deviceType = useDeviceType();
