575b7710fa52431978d1996b2a9928a0a3b9b8fe
VadimTarasenko <35116343+VadimTarasenko@users.noreply.github.com>
2024-11-07 21:37:00 +0200

    fix: upload photo dialog fixes on mobile view (#145)

--- src/modules/settings/containers/professional-settings-gallery/professional-settings-gallery.tsx
+++ src/modules/settings/containers/professional-settings-gallery/professional-settings-gallery.tsx
@@ -2,27 +2,10 @@ import { type FC } from 'react';
 // components
 import { ProfileSettingsTabContentLayout } from '@/modules/user/components/profile-settings-tab-content-layout';
 import { ProfessionalGalleryForm } from '@/modules/user/components/professional-gallery-form';
-import { AlbumAddModal } from '@/modules/settings/components/album-add-modal';
-import { Button } from '@/modules/core/components/button';
-import { useBoolean } from 'usehooks-ts';
 
 export const ProfessionalSettingsGallery: FC = () => {
-  const isModalOpen = useBoolean();
-
   return (
     <ProfileSettingsTabContentLayout
-      actions={[
-        {
-          actionNode: (
-            <AlbumAddModal
-              onOpenChange={isModalOpen.setValue}
-              isOpen={isModalOpen.value}
-              trigger={<Button text='Add album' />}
-            />
-          ),
-          isMobile: true,
-        },
-      ]}
       title='gallery.settings.title'
       icon='folder'
     >
