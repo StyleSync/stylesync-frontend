ddc2ed59bb40a0fd08a8dbfd9a7db68ec593253f
Derii Eugene <eugenederii@gmail.com>
2024-12-04 13:40:30 +0200

    feat: added new changes

--- src/modules/schedule/components/date-slider-calendar/date-slider-calendar.tsx
+++ src/modules/schedule/components/date-slider-calendar/date-slider-calendar.tsx
@@ -3,18 +3,67 @@ import { Typography } from '@/modules/core/components/typogrpahy';
 
 import styles from '@/modules/booking/containers/booking-time-select/booking-time-select.module.scss';
 import { type DateSliderCalendarProps } from './date-slider-calendar.inerface';
-import { type FC } from 'react';
+import { useEffect, useState, type FC } from 'react';
 import { format } from 'date-fns';
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Swiper styles
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 
-export const DateSliderCalendar: FC<DateSliderCalendarProps> = ({ days }) => {
+export const DateSliderCalendar: FC<DateSliderCalendarProps> = ({
+  days,
+  swiperRef,
+  onSwiper,
+  onDateSelect,
+  selectedDate,
+}) => {
+  const [today, setToday] = useState<Date>(new Date());
+
+  useEffect(() => {
+    setToday(new Date());
+  }, []);
+
+  const todaySlideClass = (day: Date) => {
+    const isSameDay =
+      day.getFullYear() === today.getFullYear() &&
+      day.getMonth() === today.getMonth() &&
+      day.getDate() === today.getDate();
+
+    return isSameDay ? '!bg-primary' : '';
+  };
+
+  const selectedSlideClass = (day: Date) => {
+    return selectedDate && day.toISOString() === selectedDate.toISOString()
+      ? '!bg-primary'
+      : '';
+  };
+
+  const todaySlideClassText = (day: Date) => {
+    const isSameDay =
+      day.getFullYear() === today.getFullYear() &&
+      day.getMonth() === today.getMonth() &&
+      day.getDate() === today.getDate();
+
+    return isSameDay ? '!text-white' : '';
+  };
+
+  const selectedDateClassText = (day: Date) => {
+    return selectedDate && day.toISOString() === selectedDate.toISOString()
+      ? '!text-white'
+      : '';
+  };
+
+  const handleDateSelect = (day: Date) => {
+    onDateSelect(day);
+    setToday(day);
+  };
+
   if (!days || days.length === 0) return;
 
   return (
     <Swiper
+      onSwiper={onSwiper}
+      ref={swiperRef}
       spaceBetween={10}
       loop={false}
       slidesPerView='auto'
@@ -22,10 +71,23 @@ export const DateSliderCalendar: FC<DateSliderCalendarProps> = ({ days }) => {
       className='z-10 w-full !pb-4'
     >
       {days.map((day, index) => (
-        <SwiperSlide key={index} className={styles.swiperSlideCalendar}>
-          <div className='flex w-full flex-col items-center gap-3 rounded-xl bg-white py-[11px] pb-3 shadow'>
-            <Typography>{format(day, 'EEE')}</Typography>
-            <Typography>{format(day, 'd')}</Typography>
+        <SwiperSlide key={index} className={`${styles.swiperSlideCalendar}`}>
+          <div
+            className={`flex w-full flex-col items-center gap-3 rounded-xl bg-white py-[11px] pb-3 shadow ${todaySlideClass(day)} ${selectedSlideClass(day)}`}
+            onClick={() => handleDateSelect(day)}
+          >
+            <Typography
+              className={`${todaySlideClassText(day)} ${selectedDateClassText(day)}`}
+              variant='body2'
+            >
+              {format(day, 'EEE')}
+            </Typography>
+            <Typography
+              className={`${todaySlideClassText(day)} ${selectedDateClassText(day)} `}
+              weight='semibold'
+            >
+              {format(day, 'd')}
+            </Typography>
             <div className='flex gap-1'>
               <div className='h-[5px] w-[5px] rounded-full bg-green-500' />
               <div className='h-[5px] w-[5px] rounded-full bg-green-500' />
--- src/modules/schedule/components/mobile-calendar/calendar-mobile.tsx
+++ src/modules/schedule/components/mobile-calendar/calendar-mobile.tsx
@@ -1,24 +1,139 @@
-import dynamic from 'next/dynamic';
-import { type FC, useEffect, useState } from 'react';
-import { type CalendarMobileProps } from './calendar-mobile.interface';
+// import dynamic from 'next/dynamic';
+import {
+  type FC,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  useMemo,
+} from 'react';
+import clsx from 'clsx';
+import { trpc } from '@/modules/core/utils/trpc.utils';
+import { eachDayOfInterval, endOfMonth, format, startOfMonth } from 'date-fns';
+import { useIntl } from 'react-intl';
+// fullcalendar
 import dayGridPlugin from '@fullcalendar/daygrid';
-// import listPlugin from '@fullcalendar/list';
 import timeGridPlugin from '@fullcalendar/timegrid';
-// import { useIntl } from 'react-intl';
-// import { formatI18n } from '@/modules/internationalization/utils/data-fns-internationalization';
+import type { CalendarApi, EventInput } from '@fullcalendar/core';
+// components
+import { Icon } from '@/modules/core/components/icon';
 import './calendar-mobile';
 import { DateSelectCalendar } from '@/modules/schedule/components/data-select-calendar';
 import { DateSliderCalendar } from '../date-slider-calendar';
-import { eachDayOfInterval, endOfMonth, startOfMonth } from 'date-fns';
+// utils
+import { formatI18n } from '@/modules/internationalization/utils/data-fns-internationalization';
+// constants
+import { weekdays } from '@/modules/schedule/constants/schedule.constants';
+// type
+import { type Swiper } from 'swiper/types';
+import { type CalendarMobileProps } from './calendar-mobile.interface';
+import styles from '@/modules/schedule/components/calendar/calendarEvent.module.scss';
 
-const FullCalendar = dynamic(() => import('@fullcalendar/react'), {
-  ssr: false,
-});
+import FullCalendar from '@fullcalendar/react';
+
+// const FullCalendar = dynamic(() => import('@fullcalendar/react'), {
+//   ssr: false,
+// });
+
+const SPEED_TO_SLIDE = 500;
 
 export const CalendarMobile: FC<CalendarMobileProps> = () => {
-  // const intl = useIntl();
+  const intl = useIntl();
 
   const [selectedDates, setSelectedDates] = useState<Date[]>();
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const swiperRef = useRef<Swiper | null>(null);
+  const calendarRef = useRef<FullCalendar>(null);
+
+  // queries
+  const [me] = trpc.user.me.useSuspenseQuery({ expand: ['professional'] });
+  const [events] = trpc.booking.list.useSuspenseQuery({
+    expand: ['serviceProfessional'],
+    professionalId: me.professional?.id,
+  });
+
+  const { data: weekSchedule } = trpc.schedule.getWeekSchedule.useQuery(
+    {
+      professionalId: me?.professional?.id ?? '',
+    },
+    {
+      enabled: Boolean(me?.professional),
+    }
+  );
+
+  const businessHours: EventInput[] = useMemo(() => {
+    if (!weekSchedule) return [];
+
+    return weekSchedule.map((schedule) => {
+      const dayOfWeek =
+        weekdays.findIndex((weekDay) => weekDay === schedule.day) + 1;
+
+      return {
+        daysOfWeek: [dayOfWeek],
+        startTime: formatI18n(schedule.start, 'H:mm', intl.locale),
+        endTime: formatI18n(schedule.end, 'H:mm', intl.locale),
+      };
+    });
+  }, [weekSchedule, intl.locale]);
+
+  const eventsList = useMemo(() => {
+    return events.map((event) => ({
+      id: event.id,
+      title: event.serviceProfessional.title,
+      start: new Date(event.startTime),
+      end: new Date(event.endTime),
+      status: event.status,
+      className: clsx(styles.event, styles[`event_${event.status}`]),
+    }));
+  }, [events]);
+
+  useEffect(() => {
+    const calendarApi: CalendarApi =
+      calendarRef.current?.getApi() as CalendarApi;
+
+    if (!calendarApi) {
+      return;
+    }
+
+    // setTimeout(() => {
+    //   if (selectedDate) {
+    //     calendarApi.gotoDate(selectedDate);
+    //   }
+    // }, 0);
+    queueMicrotask(() => {
+      if (selectedDate) {
+        calendarApi.gotoDate(selectedDate);
+      }
+    });
+  }, [selectedDate]);
+
+  // scroll to slide
+  const onSwiperHandler = useCallback(
+    (swiper: Swiper) => {
+      if (swiperRef.current !== swiper) {
+        swiperRef.current = swiper;
+      }
+    },
+    [swiperRef]
+  );
+
+  //
+  useEffect(() => {
+    if (
+      selectedDate &&
+      Array.isArray(selectedDates) &&
+      selectedDates?.length > 0 &&
+      swiperRef.current
+    ) {
+      const index = selectedDates.findIndex(
+        (date) => date.toISOString() === selectedDate.toISOString()
+      );
+
+      if (index !== -1) {
+        swiperRef.current.slideTo(index, SPEED_TO_SLIDE);
+      }
+    }
+  }, [selectedDate, selectedDates]);
 
   useEffect(() => {
     const today = new Date();
@@ -27,19 +142,32 @@ export const CalendarMobile: FC<CalendarMobileProps> = () => {
     const daysOfMonth = eachDayOfInterval({ start: startDay, end: endDay });
 
     setSelectedDates(daysOfMonth);
+    setSelectedDate(today);
   }, []);
 
   return (
-    <div className='relative flex w-full flex-col gap-5'>
+    <div className='relative flex w-full flex-col gap-2'>
       <div className='pl-6'>
-        <DateSelectCalendar onMonthChange={setSelectedDates} />
+        <DateSelectCalendar
+          onDateSelect={setSelectedDate}
+          onMonthChange={setSelectedDates}
+          selectedDate={selectedDate}
+        />
       </div>
-      <div className='relative flex w-full max-w-full pb-4'>
-        <DateSliderCalendar days={selectedDates || []} />
+      <div className='relative flex w-full max-w-full'>
+        <DateSliderCalendar
+          onSwiper={onSwiperHandler}
+          swiperRef={swiperRef}
+          days={selectedDates || []}
+          onDateSelect={setSelectedDate}
+          selectedDate={selectedDate}
+        />
       </div>
 
       <div className='h-full border-t border-primary-light pl-6'>
         <FullCalendar
+          events={eventsList}
+          ref={calendarRef}
           plugins={[dayGridPlugin, timeGridPlugin]}
           initialView='timeGridDay'
           buttonText={{ today: 'Today' }}
@@ -51,9 +179,42 @@ export const CalendarMobile: FC<CalendarMobileProps> = () => {
             omitZeroMinute: false,
             meridiem: false,
           }}
+          businessHours={businessHours}
           allDaySlot={false}
           height={'75vh'}
           nowIndicator
+          eventContent={({ event }) => {
+            const startTime = event.start
+              ? format(new Date(event.start), 'HH:mm')
+              : null;
+            const endTime = event.end
+              ? format(new Date(event.end), 'HH:mm')
+              : null;
+
+            return (
+              <div
+                key={event.id}
+                className='relative flex flex-col gap-y-1 pl-3'
+              >
+                <div className='flex'>
+                  <span className='truncate text-base font-medium text-white'>
+                    {event.title}
+                  </span>
+                </div>
+                <div className='flex items-center gap-x-1'>
+                  <Icon
+                    name='time'
+                    width={16}
+                    height={16}
+                    className='text-white'
+                  />
+                  <span className='text-sm font-medium text-white'>
+                    {startTime} - {endTime}
+                  </span>
+                </div>
+              </div>
+            );
+          }}
         />
       </div>
     </div>
