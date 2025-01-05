e0b1b2b99a230185d84df03a9dd69944720a89a7
eugenederii <109093189+eugenederii@users.noreply.github.com>
2025-01-05 15:30:13 +0200

    feat: add privacy and terms policy (#179)

--- src/modules/landing/containers/footer/footer.tsx
+++ src/modules/landing/containers/footer/footer.tsx
@@ -1,3 +1,4 @@
+'use client';
 // components
 import { Typography } from '@/modules/core/components/typogrpahy';
 import { Divider } from '@/modules/core/components/divider';
@@ -8,13 +9,23 @@ export const Footer = () => {
     <footer className='relative z-10 flex w-full flex-col bg-[#0F1019]'>
       <div className='pb-[110px] pl-[100px] pr-[100px] pt-20'>
         <div className='flex flex-col items-center gap-[30px] lg:flex-row lg:justify-between'>
-          <div className='flex gap-[36px]'>
-            <Typography className='whitespace-nowrap !text-[#767A85] duration-200 hover:cursor-pointer hover:!text-white'>
+          <div className='flex items-center gap-[36px]'>
+            <span
+              onClick={() => {
+                window.open('/app/privacy-policy', '_blank');
+              }}
+              className='whitespace-nowrap !text-[#767A85] duration-200 hover:cursor-pointer hover:!text-white'
+            >
               Privacy & Police
-            </Typography>
-            <Typography className='whitespace-nowrap !text-[#767A85] duration-200 hover:cursor-pointer hover:!text-white'>
+            </span>
+            <span
+              onClick={() => {
+                window.open('/app/terms-policy', '_blank');
+              }}
+              className='whitespace-nowrap !text-[#767A85] duration-200 hover:cursor-pointer hover:!text-white'
+            >
               Terms & conditions
-            </Typography>
+            </span>
           </div>
           <Icon
             className='lg:mr-[140px]'
