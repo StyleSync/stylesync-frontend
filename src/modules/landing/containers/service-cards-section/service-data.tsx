c90ae24cc61390cfcb3a0d999bda4d81ea78e8f4
eugenederii <109093189+eugenederii@users.noreply.github.com>
2025-01-12 15:55:28 +0200

    feat: update images (#186)

--- src/modules/landing/containers/service-cards-section/service-data.tsx
+++ src/modules/landing/containers/service-cards-section/service-data.tsx
@@ -1,10 +1,11 @@
-import nails from '@/modules/landing/landing-image/nails.png';
-import hair from '@/modules/landing/landing-image/hair.png';
-import finess from '@/modules/landing/landing-image/fitness.png';
-import makeup from '@/modules/landing/landing-image/makeup.png';
-import skincare from '@/modules/landing/landing-image/skincare.jpeg';
-import epil from '@/modules/landing/landing-image/epil.png';
+import nails from '@/modules/landing/landing-image/nails.jpg';
+import hair from '@/modules/landing/landing-image/hair.jpg';
+import finess from '@/modules/landing/landing-image/fitness.jpg';
+import makeup from '@/modules/landing/landing-image/makeup.jpg';
+import skincare from '@/modules/landing/landing-image/skincare.jpg';
+import massage from '@/modules/landing/landing-image/massage.jpg';
 import psychology from '@/modules/landing/landing-image/psychology.jpg';
+import eyebrows from '@/modules/landing/landing-image/eyebrows.jpg';
 
 export const services = [
   {
@@ -16,25 +17,30 @@ export const services = [
     image: hair,
   },
   {
-    title: 'general.service.massage',
-    image: epil,
+    title: 'general.service.nails',
+    image: nails,
   },
+
   {
     title: 'general.service.fitness',
     image: finess,
   },
+
   {
-    title: 'general.service.nails',
-    image: nails,
+    title: 'general.service.eyebrows',
+    image: eyebrows,
   },
+
+  {
+    title: 'general.service.massage',
+    image: massage,
+  },
+
   {
     title: 'general.service.skincare',
     image: skincare,
   },
-  {
-    title: 'general.service.eyebrows',
-    image: epil,
-  },
+
   {
     title: 'general.service.psychology',
     image: psychology,
