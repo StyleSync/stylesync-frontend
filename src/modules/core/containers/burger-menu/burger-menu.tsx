f016f3563fb23caf60e09df2c314f91cce08ed73
Derii Eugene <eugenederii@gmail.com>
2025-01-15 02:05:57 +0200

    feat: fix path

--- src/modules/core/containers/burger-menu/burger-menu.tsx
+++ src/modules/core/containers/burger-menu/burger-menu.tsx
@@ -12,7 +12,7 @@ import { Avatar } from '@/modules/core/components/avatar';
 import { Emoji } from '@/modules/core/components/emoji';
 import { DialogFullScreen } from '@/modules/core/components/dialog-full-screen';
 import { Placeholder } from '@/modules/core/components/placeholder';
-import { PfrofileLinksModal } from '@/modules/user/components/modal-profile-links';
+import { ProfileLinksModal } from '@/modules/user/components/profile-links-modal';
 // utils
 import { trpc } from '@/modules/core/utils/trpc.utils';
 import { getFullName } from '@/modules/user/utils/user.utils';
@@ -214,7 +214,7 @@ export const BurgerMenu: FC<BurgerMenuProps> = ({ session }) => {
 
   return (
     <>
-      <PfrofileLinksModal
+      <ProfileLinksModal
         isOpen={isOpenModalLinks.value}
         onOpenChange={isOpenModalLinks.setValue}
       />
