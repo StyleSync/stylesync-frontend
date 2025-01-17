d5e7c86e39f6f2f4f323778b45c53e5460eeff1a
Derii Eugene <eugenederii@gmail.com>
2025-01-17 13:36:30 +0200

    feat: add navigation to landing swiper

--- src/modules/landing/containers/service-cards-section/service-data.tsx
+++ src/modules/landing/containers/service-cards-section/service-data.tsx
@@ -9,39 +9,47 @@ import eyebrows from '@/modules/landing/landing-image/eyebrows.jpg';
 
 export const services = [
   {
+    id: 'makeup',
     title: 'general.service.makeup',
     image: makeup,
   },
   {
+    id: 'hair',
     title: 'general.service.hair',
     image: hair,
   },
   {
+    id: 'nails',
     title: 'general.service.nails',
     image: nails,
   },
 
   {
+    id: 'finess',
     title: 'general.service.fitness',
     image: finess,
   },
 
   {
+    id: 'eyebrows',
     title: 'general.service.eyebrows',
     image: eyebrows,
   },
 
   {
+    id: 'massage',
     title: 'general.service.massage',
     image: massage,
   },
 
   {
+    id: 'skincare',
     title: 'general.service.skincare',
     image: skincare,
   },
 
   {
+    id: 'psychology',
     title: 'general.service.psychology',
     image: psychology,
   },
