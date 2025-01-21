d77bb395dd28e50be6ee80539915422d4245a887
Ihor Balan <igor.balan1998@gmail.com>
2025-01-21 14:41:57 +0200

    fix: rollback fonts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -4,16 +4,14 @@ import { GeistSans } from 'geist/font/sans';
 import type { SupportedFonts } from '@/styles/styles.types';
 import type { NextFont } from 'next/dist/compiled/@next/font';
 
-const isStorybook = process.env.STORYBOOK === 'true';
-
 const inter = Inter({
   subsets: ['latin', 'cyrillic'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 const fonts: Record<SupportedFonts, NextFont> = {
-  INTER: isStorybook ? ({ className: 'inter' } as NextFont) : inter,
-  GEIST: isStorybook ? ({ className: 'geist-sans' } as NextFont) : GeistSans,
+  INTER: inter,
+  GEIST: GeistSans,
 };
 
 export { fonts };
