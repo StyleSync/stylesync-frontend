17d2e2c59e61488b7a0673980be5b8c654e7734c
eugenederii <109093189+eugenederii@users.noreply.github.com>
2025-01-21 17:02:35 +0200

    feat: redirect authenticated users (#203)

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,3 +1,7 @@
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { authOptions } from '@/modules/auth/constants/auth-server.constants';
+
 // containers
 import { IntroSection } from '@/modules/landing/containers/intro-section';
 import { ServiceCardSection } from '@/modules/landing/containers/service-cards-section';
@@ -9,6 +13,26 @@ import { LandingHeader } from '@/modules/landing/containers/header/landing-heade
 import { BottomTabNavigation } from '@/modules/core/containers/bottom-tab-navigation';
 
 export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    if (!session.user.userType) {
+      redirect('/app/account-type');
+    }
+
+    if (!session.user.onboardingCompleted) {
+      redirect('/app/onboard');
+    }
+
+    if (session.user.userType === 'PROFESSIONAL') {
+      redirect('/app/profile');
+    }
+
+    if (session.user.userType === 'CUSTOMER') {
+      redirect('/app/my-bookings');
+    }
+  }
+
   return (
     <>
       <LandingHeader />
