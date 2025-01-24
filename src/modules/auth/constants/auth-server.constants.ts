414c28c7e0d9b743529c3ac75be76a1a132ad6db
Ihor Balan <39652204+IhorBalan@users.noreply.github.com>
2025-01-24 16:08:28 +0200

    feat: return autolink account (#205)

--- src/modules/auth/constants/auth-server.constants.ts
+++ src/modules/auth/constants/auth-server.constants.ts
@@ -12,6 +12,7 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       issuer: process.env.AUTH0_ISSUER,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   callbacks: {
