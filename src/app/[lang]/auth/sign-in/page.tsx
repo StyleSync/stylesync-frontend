447dd09021d0717505b5ab432feaa582a50cafb1
VadimTarasenko <35116343+VadimTarasenko@users.noreply.github.com>
2025-02-25 17:06:14 +0200

    feat: update language select (#234)

--- src/app/[lang]/auth/sign-in/page.tsx
+++ src/app/[lang]/auth/sign-in/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 import { useEffect } from 'react';
+
+import { useParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 
 export default function Page() {
+  const params = useParams();
+
   useEffect(() => {
-    signIn('auth0', { callbackUrl: '/app/profile' }, { prompt: 'login' });
+    signIn('auth0', {
+      callbackUrl: '/app/profile',
+      ui_locales: params.lang as string,
+    });
   }, []);
 
   return null;
