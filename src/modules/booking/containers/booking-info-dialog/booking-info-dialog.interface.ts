422c96868036c1a915bf4ec133b1f31de1f32f9a
eugenederii <109093189+eugenederii@users.noreply.github.com>
2025-03-15 11:25:11 +0200

    feat: add icon to professional search card (#248)

--- src/modules/booking/containers/booking-info-dialog/booking-info-dialog.interface.ts
+++ src/modules/booking/containers/booking-info-dialog/booking-info-dialog.interface.ts
@@ -1,5 +1,5 @@
-import type { IconName } from '@/modules/core/components/icon';
 import type { ButtonVariant } from '@/modules/core/components/button/button.interface';
+import type { IconName } from '@/modules/core/components/icon';
 
 export type Action = {
   id: string;
