60354d1336dfd1ea205315a7052309a9272d2cb6
Ihor Balan <igor.balan1998@gmail.com>
2025-05-14 12:44:30 +0300

    feat: add calendar dots for days with schedule

--- src/modules/schedule/containers/daily-schedule-section/daily-schedule-section.tsx
+++ src/modules/schedule/containers/daily-schedule-section/daily-schedule-section.tsx
@@ -7,38 +7,97 @@ import {
   type PickersDayProps,
 } from '@mui/x-date-pickers/PickersDay';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-import { isSameDay, startOfDay } from 'date-fns';
+import { Schedule } from '@prisma/client';
+import {
+  eachDayOfInterval,
+  endOfMonth,
+  isSameDay,
+  startOfDay,
+  startOfMonth,
+} from 'date-fns';
 import { enUS, uk } from 'date-fns/locale';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import { useDeviceType } from '@/modules/core/hooks/use-device-type';
+import { trpc } from '@/modules/core/utils/trpc.utils';
 import { DailyScheduleForm } from '@/modules/schedule/containers/daily-schedule-form';
 
 const renderCalendarDay = (
-  props: PickersDayProps<Date> & { selectedDates?: number[] }
+  props: PickersDayProps<Date> & {
+    selectedDates?: number[];
+    schedules?: Schedule[];
+  }
 ): ReactNode => {
   const { selectedDates = [], day, outsideCurrentMonth, ...other } = props;
 
   const isActive = selectedDates.some((date) => isSameDay(date, day));
 
+  const selectedDay = {
+    specificDay: day.getDate(),
+    specificMonth: day.getMonth(),
+    specificYear: day.getFullYear(),
+  };
+
+  const foundSchedule = props.schedules?.find(
+    (schedule) =>
+      schedule.specificDay === selectedDay.specificDay &&
+      schedule.specificMonth === selectedDay.specificMonth &&
+      schedule.specificYear === selectedDay.specificYear
+  );
+
   return (
-    <PickersDay
-      {...other}
-      outsideCurrentMonth={outsideCurrentMonth}
-      day={day}
-      className={
-        isActive ? '!bg-primary !text-white' : '!bg-transparent !text-dark'
-      }
-    />
+    <div className='relative'>
+      <PickersDay
+        {...other}
+        outsideCurrentMonth={outsideCurrentMonth}
+        day={day}
+        className={
+          isActive ? '!bg-primary !text-white' : '!bg-transparent !text-dark'
+        }
+      />
+      {foundSchedule && (
+        <div className='absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-[3px]'>
+          <div className='h-[5px] w-[5px] rounded-full bg-orange' />
+        </div>
+      )}
+    </div>
   );
 };
+
+const initialMonth = new Date();
 
 export const DailyScheduleSection = () => {
   const { locale } = useIntl();
   const deviceType = useDeviceType();
   const calendarWrapperRef = useRef<HTMLDivElement>(null);
+  const { data: me } = trpc.user.me.useQuery({
+    expand: ['professional'],
+  });
 
   const [selectedDates, setSelectedDates] = useState<Date[]>([]);
+  const [selectedMonth, setSelectedMonth] = useState<Date>(initialMonth);
+
+  const allMonthDays = useMemo(() => {
+    const start = startOfMonth(selectedMonth);
+    const end = endOfMonth(selectedMonth);
+    const days = eachDayOfInterval({ start, end });
+
+    return days.map((date) => ({
+      specificDay: date.getDate(),
+      specificMonth: date.getMonth(),
+      specificYear: date.getFullYear(),
+    }));
+  }, [selectedMonth]);
+
+  const { data: schedules } = trpc.schedule.listByDay.useQuery(
+    {
+      days: allMonthDays,
+      professionalId: me?.professional?.id ?? '',
+    },
+    {
+      enabled: !!me?.professional?.id,
+    }
+  );
 
   const dateFnsLocale = useMemo(() => {
     if (locale === 'uk') return uk;
@@ -83,7 +142,10 @@ export const DailyScheduleSection = () => {
               adapterLocale={dateFnsLocale}
             >
               <StaticDatePicker
-                value={new Date()}
+                value={selectedMonth}
+                onMonthChange={(value) => {
+                  setSelectedMonth(value);
+                }}
                 onChange={(value) => {
                   if (!value) return;
 
@@ -101,6 +163,7 @@ export const DailyScheduleSection = () => {
                 slotProps={{
                   day: {
                     selectedDates,
+                    schedules: schedules ?? [],
                   } as any,
                 }}
                 sx={{
