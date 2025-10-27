fde7070cadc0653494c6fd1a188e801631149ce4
eugenederii <109093189+eugenederii@users.noreply.github.com>
2025-10-27 14:07:53 +0200

    fix: overlay z-index (#284)

--- src/app/[lang]/app/layout.tsx
+++ src/app/[lang]/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function AppLayout({ children }: ChildrenProp) {
         blurDataURL={Bg.blurDataURL}
         alt='background'
       />
-      <div className='fixed left-0 top-0 z-[2] h-full w-full bg-gradient-to-b from-white to-transparent' />
+      <div className='pointer-events-none fixed left-0 top-0 z-0 h-full w-full bg-gradient-to-b from-white to-transparent' />
       {children}
       <BottomTabNavigation />
     </>
